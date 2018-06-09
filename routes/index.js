1e38a0800d6e73c9d3150104f63fcb24f95eca1d
lazyevilsquid <taeyang9300@daum.net>
2018-06-09 17:20:25 +0900

    [Dev]Login page

--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,8 @@ var router = express.Router();
 router.get('/', function(req, res) {
     res.render('business/index', { title: 'GAPA index page'});
 });
+
+router.get('/login', function(req, res) {
+    res.render('business/login', { title: 'GAPA Login page'});
+});
 module.exports = router;

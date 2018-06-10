d8c92e6eb938e9bc3a8eb821aacc8ef39e5c6134
kyechan99 <kyechan99@naver.com>
2018-06-10 18:50:05 +0900

    [IMPROVE] css design

--- public/js/login.js
+++ public/js/login.js
@@ -4,13 +4,13 @@ var Authentication = new Vue({
         // state === 0 : 회원가입 창
         // state === 1 : 로그인 창
         // state === 2 : 게임 선택 창
-        state: 2,
+        state: 1,
         // game === 0 : null
         // game === 1 : 옵치
         // game === 2 : 롤
         // game === 3 : 배그
         game: 0,
-        disableButton: false
+        disableButton: true
     },
     methods: {
         // 페이지 전환
@@ -27,15 +27,15 @@ var Authentication = new Vue({
         // 게임 선택(s = select)
         soverwatch : function() {
             this.game = 1;
-            disableButton = true;
+            this.disableButton = false;
         },
         slol : function() {
             this.game = 2;
-            disableButton = true;
+            this.disableButton = false;
         },
         spubg : function() {
             this.game = 3;
-            disableButton = true;
+            this.disableButton = false;
         }
     }
 })

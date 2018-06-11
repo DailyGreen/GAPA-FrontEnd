6b864348a5ef3315df2f9d1c64969b8f728f8196
kyechan99 <kyechan99@naver.com>
2018-06-11 19:48:51 +0900

    [Dev] Another Design Root

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-    res.render('business/index', { title: 'GAPA index page'});
+    res.render('business/authentication', { title: 'GAPA index page'});
 });
 
 router.get('/login', function(req, res) {
     res.render('business/login', { title: 'GAPA Login page'});
 });
+
 module.exports = router;

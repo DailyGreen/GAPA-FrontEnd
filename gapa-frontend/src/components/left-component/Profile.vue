42a0076e1ad0d311708e6c1a0087ade901c0a0e5
kyechan99 <kyechan99@gmail.com>
2020-05-28 10:46:51 +0000

    Profile Concept

--- gapa-frontend/src/components/left-component/Profile.vue
+++ gapa-frontend/src/components/left-component/Profile.vue
@@ -1,15 +1,20 @@
 <template>
 	<div>
 		<div class="profile-page">
-			<h3>Games<span> ></span></h3>
-			<div class="games">
-				<img class="game-card" :src="require(`@/assets/img/games/LeagueOfLegend.png`)"/>
-				<img class="game-card" :src="require(`@/assets/img/games/BATTLEGROUNDS.png`)"/>
+			<div class="menus">
+				<button class="btn" v-on:click="menuType = MENU_TYPE.RECENT">Recent</button>
+				<button class="btn" v-on:click="menuType = MENU_TYPE.GAMES">Games</button>
 			</div>
-			<h3>Recent<span> ></span></h3>
-			<div class="recents">
+			<!-- 	Recent : 최근 활동 기록		 -->
+			<div class="recents" v-show="menuType === MENU_TYPE.RECENT">
+				<img class="post-img" :src="require(`@/assets/img/usefull/tempRecentLog_0.png`)"/>
 				<p> 리그오브레전드를(을) 5인파티로 생성해서 게임함 (3hours)</p>
 				<p> 배틀그라운드(을) 4인 스쿼드 파티로 생성해서 게임함 (2days)</p>
+			</div>
+			<!-- 	Games : 팔로윙한 게임들		 -->
+			<div class="games" v-show="menuType === MENU_TYPE.GAMES">
+				<img class="game-card" :src="require(`@/assets/img/games/LeagueOfLegend.png`)"/>
+				<img class="game-card" :src="require(`@/assets/img/games/BATTLEGROUNDS.png`)"/>
 			</div>
 		</div>
 	</div>
@@ -38,7 +43,12 @@ export default {
 	data () {
 		return {
 			LEFT_TYPE : new LEFT_TYPE(),
-			RIGHT_TYPE : new RIGHT_TYPE()
+			RIGHT_TYPE : new RIGHT_TYPE(),
+			MENU_TYPE: {
+				RECENT: 'Recent',
+				GAMES: 'Games'
+			},
+			menuType: 'Recent'
 		}
 	},
 	components: {
@@ -53,13 +63,18 @@ export default {
 .profile-page {
 	color: black;
 	font-size: 14px;
-	h3 {
-		margin-top: 30px;
-		font-size: 20px;
-		font-weight: 700;
-		span {
-			font-size: 14px;
-			font-weight: 400;
+	.menus {
+		margin-bottom: 30px;
+		.btn {
+			font-size: 20px;
+			font-weight: 700;
+			padding: 0px;
+			margin-right: 24px;
+		}
+	}
+	.recents {
+		.post-img {
+			width: 100%;
 		}
 	}
 	.games {
